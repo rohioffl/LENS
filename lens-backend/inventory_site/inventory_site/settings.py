0b5f9f0b696330847fa3eef20580855f6ba921c2
Rohit P T <rohit.pt@ankercloud.com>
2025-12-17 13:16:19 +0530

    fix 10

--- lens-backend/inventory_site/inventory_site/settings.py
+++ lens-backend/inventory_site/inventory_site/settings.py
@@ -12,9 +12,6 @@ from pathlib import Path
 # --------------------------------------------------
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print("🔥 SETTINGS FILE LOADED 🔥")
-print("ALLOWED_HOSTS =", ALLOWED_HOSTS)
-
 # --------------------------------------------------
 # SECURITY
 # --------------------------------------------------
